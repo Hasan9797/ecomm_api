ee0175a32c6990d87f91c4d650cf8292259dd54f
hasan97 <hasanjonsadullyev9@gmail.com>
2024-05-25 18:19:31 +0500

    deploy start

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Anvarchik :) !");
 });
 
 const PORT = process.env.PORT || 8000;
