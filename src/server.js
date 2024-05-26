edfd35149ac3e8dd585c2abb0d70702ce0b321d6
Hasan <hasanjonsadullayev97@gmail.com>
2024-05-26 14:35:26 +0500

    +-

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello Anvarchik :) !");
+app.get('/', (req, res) => {
+	res.send('Hello MyAPI :) !');
 });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}:...`);
+	console.log(`Server running on port ${PORT}:...`);
 });
