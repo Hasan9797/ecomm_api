c8c087e5614c569968ceaf10865b06bc66e1b693
hasan97 <hasanjonsadullyev9@gmail.com>
2024-05-28 19:29:26 +0500

    connected DB

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
-import express from 'express';
+import express from "express";
+import env from "dotenv";
+import { connectDB } from "./connections/connect.js";
 
 const app = express();
+env.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.send('Hello Anvar tog`ooo :) !');
-});
+app.use("/api/category");
+app.use("/api/product");
+app.use("/api/order");
+app.use("/api/user");
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}:...`);
+  console.log(`Server running on port ${PORT}:...`);
+  connectDB();
 });
