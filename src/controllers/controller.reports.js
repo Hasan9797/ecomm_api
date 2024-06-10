884ee532b39fb17d0228ed65fd2225a0a5a4dde9
Hasan <hasanjonsadullayev97@gmail.com>
2024-06-10 21:58:09 +0500

    ++

--- src/controllers/controller.reports.js
+++ src/controllers/controller.reports.js
@@ -1,5 +1,8 @@
-import { Sequelize, Op } from "sequelize";
-import dataBase from "../models/model.index.js";
+import { Sequelize, Op } from 'sequelize';
+import dataBase from '../models/model.index.js';
 const { Product, SQL } = dataBase;
 
 const getAllReports = (req, res) => {};
+const getUserReport = (req, res) => {};
+
+export default { getAllReports, getUserReport };
--- src/routers/router.order.js
+++ src/routers/router.order.js
@@ -1,9 +1,9 @@
-import Router from "express";
-import orderControllers from "../controllers/controller.order.js";
+import Router from 'express';
+import orderControllers from '../controllers/controller.order.js';
 const order = Router();
 
-order.get("/", orderControllers.getAll);
-order.get("/by/:id", orderControllers.getById);
-order.post("/add", orderControllers.create);
+order.get('/', orderControllers.getAll);
+order.get('/by/:id', orderControllers.getById);
+order.post('/add', orderControllers.create);
 
 export default order;
