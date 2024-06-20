cf42e205a9c90df4b8c7b812ec91c10a05901349
hasan97 <hasanjonsadullyev9@gmail.com>
2024-06-20 16:46:05 +0500

    ++

--- src/models/model.category.js
+++ src/models/model.category.js
@@ -1,49 +1,53 @@
-import { DataTypes } from 'sequelize';
-import db from '../connections/connection.db.js';
+import { DataTypes } from "sequelize";
+import db from "../connections/connection.db.js";
 
 const Category = db.define(
-	'category',
-	{
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		img: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		description: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		createdAt: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			get() {
-				return this.getDataValue('createdAt');
-			},
-		},
-		updatedAt: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			get() {
-				return this.getDataValue('updatedAt');
-			},
-		},
-	},
-	{
-		timestamps: true,
-		hooks: {
-			beforeCreate(order) {
-				const currentTimestamp = Math.floor(Date.now() / 1000);
-				order.createdAt = currentTimestamp;
-				order.updatedAt = currentTimestamp;
-			},
-			beforeUpdate(order) {
-				order.updatedAt = Math.floor(Date.now() / 1000);
-			},
-		},
-	}
+  "category",
+  {
+    title_uz: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title_ru: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      get() {
+        return this.getDataValue("createdAt");
+      },
+    },
+    updatedAt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      get() {
+        return this.getDataValue("updatedAt");
+      },
+    },
+  },
+  {
+    timestamps: true,
+    hooks: {
+      beforeCreate(order) {
+        const currentTimestamp = Math.floor(Date.now() / 1000);
+        order.createdAt = currentTimestamp;
+        order.updatedAt = currentTimestamp;
+      },
+      beforeUpdate(order) {
+        order.updatedAt = Math.floor(Date.now() / 1000);
+      },
+    },
+  }
 );
 
 export default Category;
--- src/models/model.product.js
+++ src/models/model.product.js
@@ -1,59 +1,75 @@
-import { DataTypes } from 'sequelize';
-import db from '../connections/connection.db.js';
-import productEnum from '../enums/product_enum.js';
+import { DataTypes } from "sequelize";
+import db from "../connections/connection.db.js";
+import productEnum from "../enums/product_enum.js";
 
 const Product = db.define(
-	'product',
-	{
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		img: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		price: {
-			type: DataTypes.BIGINT,
-			allowNull: false,
-		},
-		status: {
-			type: DataTypes.INTEGER,
-			defaultValue: productEnum.STATUS_CREATE,
-			allowNull: false,
-		},
-		description: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		createdAt: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			get() {
-				return this.getDataValue('createdAt');
-			},
-		},
-		updatedAt: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			get() {
-				return this.getDataValue('updatedAt');
-			},
-		},
-	},
-	{
-		timestamps: true,
-		hooks: {
-			beforeCreate(order) {
-				const currentTimestamp = Math.floor(Date.now() / 1000);
-				order.createdAt = currentTimestamp;
-				order.updatedAt = currentTimestamp;
-			},
-			beforeUpdate(order) {
-				order.updatedAt = Math.floor(Date.now() / 1000);
-			},
-		},
-	}
+  "product",
+  {
+    title_uz: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title_ru: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gallery: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      defaultValue: productEnum.STATUS_CREATE,
+      allowNull: false,
+    },
+    characteristic: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    description_uz: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description_ru: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      get() {
+        return this.getDataValue("createdAt");
+      },
+    },
+    updatedAt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      get() {
+        return this.getDataValue("updatedAt");
+      },
+    },
+  },
+  {
+    timestamps: true,
+    hooks: {
+      beforeCreate(order) {
+        const currentTimestamp = Math.floor(Date.now() / 1000);
+        order.createdAt = currentTimestamp;
+        order.updatedAt = currentTimestamp;
+      },
+      beforeUpdate(order) {
+        order.updatedAt = Math.floor(Date.now() / 1000);
+      },
+    },
+  }
 );
 
 export default Product;
