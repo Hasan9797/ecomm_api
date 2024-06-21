b14909dafb66d74bb7090e69c3ffcffba436d6ef
hasan97 <hasanjonsadullyev9@gmail.com>
2024-06-21 17:47:48 +0500

    ++

--- migrations/20240621054417-create-categories-table.js
+++ migrations/20240621054417-create-categories-table.js
@@ -1,78 +1,70 @@
 "use strict";
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(
-      "categories",
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        title_uz: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        title_ru: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
-        img: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        parentId: {
-          type: DataTypes.INTEGER,
-          allowNull: true, // Agar `parentId` bo'sh bo'lishi mumkin bo'lsa
-          references: {
-            model: "categories", // foreign key bo'lgan jadval nomi
-            key: "id",
-          },
-        },
-        description: {
-          type: DataTypes.TEXT,
-          allowNull: false,
-        },
-        createdAt: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          get() {
-            return this.getDataValue("createdAt");
-          },
-        },
-        updatedAt: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          get() {
-            return this.getDataValue("updatedAt");
-          },
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable(
+    "categories",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title_uz: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      title_ru: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      img: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      parentId: {
+        type: DataTypes.INTEGER,
+        allowNull: true, // Agar `parentId` bo'sh bo'lishi mumkin bo'lsa
+        references: {
+          model: "categories", // foreign key bo'lgan jadval nomi
+          key: "id",
         },
       },
-      {
-        timestamps: true,
-        hooks: {
-          beforeCreate(order) {
-            const currentTimestamp = Math.floor(Date.now() / 1000);
-            order.createdAt = currentTimestamp;
-            order.updatedAt = currentTimestamp;
-          },
-          beforeUpdate(order) {
-            order.updatedAt = Math.floor(Date.now() / 1000);
-          },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        get() {
+          return this.getDataValue("createdAt");
         },
-      }
-    );
-  },
+      },
+      updatedAt: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        get() {
+          return this.getDataValue("updatedAt");
+        },
+      },
+    },
+    {
+      timestamps: true,
+      hooks: {
+        beforeCreate(order) {
+          const currentTimestamp = Math.floor(Date.now() / 1000);
+          order.createdAt = currentTimestamp;
+          order.updatedAt = currentTimestamp;
+        },
+        beforeUpdate(order) {
+          order.updatedAt = Math.floor(Date.now() / 1000);
+        },
+      },
+    }
+  );
+}
 
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  },
-};
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable("categories");
+}
