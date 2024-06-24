576cae42e0129556136fc2bdf7f31ee3d03d77f4
hasan97 <hasanjonsadullyev9@gmail.com>
2024-06-24 11:43:32 +0500

    sdds

--- src/controllers/controller.category.js
+++ src/controllers/controller.category.js
@@ -1,136 +1,136 @@
-import dataBase from '../models/model.index.js';
-import { unlinkFile } from '../helpers/fileHelper.js';
+import dataBase from "../models/model.index.js";
+import { unlinkFile } from "../helpers/fileHelper.js";
 const { Category } = dataBase;
 
 const getAll = async (req, res) => {
-	const lang = req.headers['accept-language'];
-	try {
-		const categories = await Category.findAll({
-			include: [
-				{
-					model: Category,
-					as: 'subcategories',
-				},
-			],
-		});
+  const lang = req.headers["accept-language"];
+  try {
+    const categories = await Category.findAll({
+      include: [
+        {
+          model: Category,
+          as: "subcategories",
+        },
+      ],
+    });
 
-		if (categories.length <= 0) {
-			return res.status(200).json({ message: 'No categories', data: [] });
-		}
+    if (categories.length <= 0) {
+      return res.status(200).json({ message: "No categories", data: [] });
+    }
 
-		const langCategory = categories.map(category => {
-			return {
-				id: category.id,
-				title: lang === 'ru' ? category.title_ru : category.title_uz,
-				img: category.img,
-				createdAt: category.createdAt,
-				updatedAt: category.updatedAt,
-				subcategories: category.subcategories.map(sub => {
-					return {
-						id: sub.id,
-						title: lang === 'ru' ? sub.title_ru : sub.title_uz,
-						img: category.img,
-						createdAt: sub.createdAt,
-						updatedAt: sub.updatedAt,
-					};
-				}),
-			};
-		});
-		res.status(200).json({ message: 'Success', data: langCategory });
-	} catch (error) {
-		throw new Error(error);
-	}
+    const langCategory = categories.map((category) => {
+      return {
+        id: category.id,
+        title: lang === "ru" ? category.title_ru : category.title_uz,
+        img: category.img,
+        createdAt: category.createdAt,
+        updatedAt: category.updatedAt,
+        subcategories: category.subcategories.map((sub) => {
+          return {
+            id: sub.id,
+            title: lang === "ru" ? sub.title_ru : sub.title_uz,
+            img: category.img,
+            createdAt: sub.createdAt,
+            updatedAt: sub.updatedAt,
+          };
+        }),
+      };
+    });
+    res.status(200).json({ message: "Success", data: langCategory });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const getById = async (req, res) => {
-	try {
-		const category = await Category.findByPk(req.params.id, {
-			include: [
-				{
-					model: Category,
-					as: 'subcategories',
-				},
-			],
-		});
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+          as: "subcategories",
+        },
+      ],
+    });
 
-		if (!category) {
-			return res.status(200).json({ message: 'Category not found', data: {} });
-		}
+    if (!category) {
+      return res.status(200).json({ message: "Category not found", data: {} });
+    }
 
-		res
-			.status(200)
-			.json({ message: 'Get category successfully', data: category });
-	} catch (error) {
-		console.error('Error fetching category with subcategories:', error);
-		throw new Error(error);
-	}
+    res
+      .status(200)
+      .json({ message: "Get category successfully", data: category });
+  } catch (error) {
+    console.error("Error fetching category with subcategories:", error);
+    throw new Error(error);
+  }
 };
 
 const create = async (req, res) => {
-	const start = Date.now();
-	try {
-		const { title_uz, title_ru } = req.body;
+  const start = Date.now();
+  try {
+    const { title_uz, title_ru } = req.body;
 
-		const img = req.file.img;
+    const img = req.file;
 
-		const newCategory = {
-			title_uz,
-			title_ru,
-			img: img ? '/' + img.filename : null,
-		};
+    const newCategory = {
+      title_uz,
+      title_ru,
+      img: img ? "/" + img.filename : null,
+    };
 
-		const category = await Category.create(newCategory);
+    const category = await Category.create(newCategory);
 
-		const end = Date.now();
-		// console.log(`Category creation took ${end - start}ms`);
+    const end = Date.now();
+    // console.log(`Category creation took ${end - start}ms`);
 
-		res.status(201).json({
-			creating: end - start + 'ms',
-			message: 'Category created successfully',
-			data: category,
-		});
-	} catch (error) {
-		console.error('Error creating Category:', error);
-		res
-			.status(500)
-			.json({ message: 'Error creating Category', error: error.message });
-	}
+    res.status(201).json({
+      creating: end - start + "ms",
+      message: "Category created successfully",
+      data: category,
+    });
+  } catch (error) {
+    console.error("Error creating Category:", error);
+    res
+      .status(500)
+      .json({ message: "Error creating Category", error: error.message });
+  }
 };
 
 const update = async (req, res) => {
-	try {
-		const newCategory = {
-			...req.body,
-		};
+  try {
+    const newCategory = {
+      ...req.body,
+    };
 
-		if (req.file) {
-			newCategory.img = '/' + req.file.filename;
-			const currentFile = await Category.findByPk(req.params.id);
-			if (currentFile && currentFile.img) {
-				unlinkFile([currentFile.img.toString().slice(1)]);
-			}
-		}
+    if (req.file) {
+      newCategory.img = "/" + req.file.filename;
+      const currentFile = await Category.findByPk(req.params.id);
+      if (currentFile && currentFile.img) {
+        unlinkFile([currentFile.img.toString().slice(1)]);
+      }
+    }
 
-		const category = await Category.update(newCategory, {
-			where: { id: req.params.id },
-		});
+    const category = await Category.update(newCategory, {
+      where: { id: req.params.id },
+    });
 
-		if (category[0] === 0) {
-			return res
-				.status(200)
-				.json({ message: 'Category not fount', data: category });
-		}
-		res.status(200).json({ message: 'Updated Successfully', data: category });
-	} catch (error) {
-		throw new Error(error);
-	}
+    if (category[0] === 0) {
+      return res
+        .status(200)
+        .json({ message: "Category not fount", data: category });
+    }
+    res.status(200).json({ message: "Updated Successfully", data: category });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const destroy = async (req, res) => {
-	await Category.destroy({
-		where: { id: req.params.id },
-	});
-	res.status(200).json({ message: 'Deleted successfully', data: true });
+  await Category.destroy({
+    where: { id: req.params.id },
+  });
+  res.status(200).json({ message: "Deleted successfully", data: true });
 };
 
 export default { getAll, getById, create, update, destroy };
--- src/helpers/fileHelper.js
+++ src/helpers/fileHelper.js
@@ -1,71 +1,71 @@
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const uploadFolderPath = path.join(__dirname, '../../', 'Uploads');
+const uploadFolderPath = path.join(__dirname, "../../", "Uploads");
 
 // Fayllarni saqlash konfiguratsiyasi
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, uploadFolderPath);
-	},
-	filename: function (req, file, cb) {
-		const uniqueSuffix = `${Date.now()}-${file.originalname
-			.trim()
-			.replace(/\s+/g, '-')}`;
-		cb(null, uniqueSuffix);
-	},
+  destination: function (req, file, cb) {
+    cb(null, uploadFolderPath);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = `${Date.now()}-${file.originalname
+      .trim()
+      .replace(/\s+/g, "-")}`;
+    cb(null, uniqueSuffix);
+  },
 });
 
 function checkfile(file, cb) {
-	const filetypes = /jpeg|jpg|png|gif/;
-	const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-	const mimetype = filetypes.test(file.mimetype);
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-	if (extname && mimetype) {
-		return cb(null, true);
-	} else {
-		cb(new Error('Error: You can only upload image files'));
-	}
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Error: You can only upload image files"));
+  }
 }
 
 export const upload = multer({
-	storage: storage,
-	limits: { fileSize: 100 * 1024 * 1024 }, // Fayl hajmi limiti 100MB
-	fileFilter: function (req, file, cb) {
-		checkfile(file, cb);
-	},
+  storage: storage,
+  limits: { fileSize: 100 * 1024 * 1024 }, // Fayl hajmi limiti 100MB
+  fileFilter: function (req, file, cb) {
+    checkfile(file, cb);
+  },
 });
 
 // Fayl o'chirish funksiyasi
 export const unlinkFile = (unlinkFiles = []) => {
-	const imageExtensions = /\.(jpg|jpeg|png|gif)$/i;
+  const imageExtensions = /\.(jpg|jpeg|png|gif)$/i;
 
-	fs.readdir(uploadFolderPath, (err, files) => {
-		if (err) {
-			console.error(
-				"Upload papkasidagi fayllarni o'qishda xatolik yuz berdi:",
-				err
-			);
-			return;
-		}
+  fs.readdir(uploadFolderPath, (err, files) => {
+    if (err) {
+      console.error(
+        "Upload papkasidagi fayllarni o'qishda xatolik yuz berdi:",
+        err
+      );
+      return;
+    }
 
-		const imageFiles = files.filter(
-			file => imageExtensions.test(file) && unlinkFiles.includes(file)
-		);
+    const imageFiles = files.filter(
+      (file) => imageExtensions.test(file) && unlinkFiles.includes(file)
+    );
 
-		imageFiles.forEach(file => {
-			const filePath = path.join(uploadFolderPath, file);
+    imageFiles.forEach((file) => {
+      const filePath = path.join(uploadFolderPath, file);
 
-			fs.unlink(filePath, err => {
-				if (err) {
-					console.error(`${file} faylini o'chirishda xatolik yuz berdi:`, err);
-				} else {
-					console.log(`${file} muvaffaqiyatli o'chirildi.`);
-				}
-			});
-		});
-	});
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(`${file} faylini o'chirishda xatolik yuz berdi:`, err);
+        } else {
+          console.log(`${file} muvaffaqiyatli o'chirildi.`);
+        }
+      });
+    });
+  });
 };
