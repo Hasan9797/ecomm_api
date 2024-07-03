e9b2b2d4838d1bc17f390754d0ba263189c2b23f
hasan97 <hasanjonsadullyev9@gmail.com>
2024-07-03 11:19:23 +0500

    sda

--- src/helpers/telegramBotHelper.js
+++ src/helpers/telegramBotHelper.js
@@ -1,9 +1,16 @@
 import axios from "axios";
-async function sendTelegramMessage(botToken, chatId, message) {
+import dotenv from "dotenv";
+
+dotenv.config();
+const botToken = process.env.TG_BOT_TOKEN;
+const chatId = process.env.TG_CHAT_ID;
+
+async function sendTelegramMessage(message) {
   const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
   const payload = {
     chat_id: chatId,
     text: message,
+    parse_mode: "HTML",
   };
 
   try {
@@ -18,4 +25,4 @@ async function sendTelegramMessage(botToken, chatId, message) {
   }
 }
 
-export default sendTelegramMessage;
+export default { sendTelegramMessage };
