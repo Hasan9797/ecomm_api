719cd5b687a0aad9f72a97f93192e985f91366ae
Hasan <hasanjonsadullayev97@gmail.com>
2024-07-21 21:00:10 +0500

    sdsds

--- src/routers/router.brand.js
+++ src/routers/router.brand.js
@@ -1,12 +1,23 @@
 import { Router } from 'express';
 import brandControllers from '../controllers/controller.brand.js';
+import { authenticateToken } from '../middlewares/verfiy.js';
 import { upload } from '../helpers/fileHelper.js';
 const route = Router();
 
 route.get('/', brandControllers.getAll);
 route.get('/byId/:id', brandControllers.getById);
-route.post('/add', upload.single('img'), brandControllers.create);
-route.post('/update/:id', upload.single('img'), brandControllers.update);
-route.delete('/delete/:id', brandControllers.destroy);
+route.post(
+	'/add',
+	authenticateToken,
+	upload.single('img'),
+	brandControllers.create
+);
+route.post(
+	'/update/:id',
+	authenticateToken,
+	upload.single('img'),
+	brandControllers.update
+);
+route.delete('/delete/:id', authenticateToken, brandControllers.destroy);
 
 export default route;
--- src/routers/router.order.js
+++ src/routers/router.order.js
@@ -1,12 +1,21 @@
 import Router from 'express';
 import orderControllers from '../controllers/controller.order.js';
+import { authenticateToken } from '../middlewares/verfiy.js';
 const order = Router();
 
-order.get('/', orderControllers.getAll);
-order.get('/by/:id', orderControllers.getById);
-order.get('/filter-code', orderControllers.getOrdersByProductCode);
-order.get('/get-by-user-name', orderControllers.getOrderByUserName);
-order.post('/add', orderControllers.create);
-order.put('/update/:id', orderControllers.update);
+order.get('/', authenticateToken, orderControllers.getAll);
+order.get('/by/:id', authenticateToken, orderControllers.getById);
+order.get(
+	'/filter-code',
+	authenticateToken,
+	orderControllers.getOrdersByProductCode
+);
+order.get(
+	'/get-by-user-name',
+	authenticateToken,
+	orderControllers.getOrderByUserName
+);
+order.post('/add', authenticateToken, orderControllers.create);
+order.put('/update/:id', authenticateToken, orderControllers.update);
 
 export default order;
