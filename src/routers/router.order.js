40d9eb57a046d7c19fc8e9d36a19147a3685eda7
Hasan <hasanjonsadullayev97@gmail.com>
2024-07-21 20:21:58 +0500

    sdds

--- src/routers/router.order.js
+++ src/routers/router.order.js
@@ -5,6 +5,7 @@ const order = Router();
 order.get('/', orderControllers.getAll);
 order.get('/by/:id', orderControllers.getById);
 order.get('/filter-code', orderControllers.getOrdersByProductCode);
+order.get('/get-by-user-name', orderControllers.getOrderByUserName);
 order.post('/add', orderControllers.create);
 order.put('/update/:id', orderControllers.update);
 
