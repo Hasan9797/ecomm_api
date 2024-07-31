f671ca90a28725a2e57fdb4113ba269ebed09fe9
hasan97 <hasanjonsadullyev9@gmail.com>
2024-07-31 16:31:41 +0500

    ssdds

--- src/repositories/repo.reports.js
+++ src/repositories/repo.reports.js
@@ -1,58 +1,58 @@
-import { Sequelize, Op } from 'sequelize';
-import dataBase from '../models/model.index.js';
+import { Sequelize, Op } from "sequelize";
+import dataBase from "../models/model.index.js";
 const { Order } = dataBase;
 
 const getAllReport = async (startDate, endDate) => {
-	try {
-		const result = await Order.findAll({
-			attributes: [
-				'status',
-				[Sequelize.fn('COUNT', Sequelize.col('id')), 'total_orders'],
-				[
-					Sequelize.fn('JSON_AGG', Sequelize.literal('ROW_TO_JSON("order")')),
-					'orders',
-				],
-			],
-			where: {
-				createdAt: {
-					[Op.between]: [startDate, endDate],
-				},
-			},
-			group: ['status'],
-		});
+  try {
+    const result = await Order.findAll({
+      attributes: [
+        "status",
+        [Sequelize.fn("COUNT", Sequelize.col("id")), "total_orders"],
+        [
+          Sequelize.fn("JSON_AGG", Sequelize.literal('ROW_TO_JSON("Order")')),
+          "orders",
+        ],
+      ],
+      where: {
+        created_at: {
+          [Op.between]: [startDate, endDate],
+        },
+      },
+      group: ["status"],
+    });
 
-		return result;
-	} catch (error) {
-		console.error('Error fetching order stats:', error);
-		throw error;
-	}
+    return result;
+  } catch (error) {
+    console.error("Error fetching order stats:", error);
+    throw error;
+  }
 };
 
 const getReportByUser = async (startDate, endDate, userNumber) => {
-	try {
-		const result = await Order.findAll({
-			attributes: [
-				'status',
-				[Sequelize.fn('COUNT', Sequelize.col('id')), 'total_orders'],
-				[
-					Sequelize.fn('JSON_AGG', Sequelize.literal('ROW_TO_JSON("order")')),
-					'orders',
-				],
-			],
-			where: {
-				createdAt: {
-					[Op.between]: [startDate, endDate],
-				},
-				user_number: userNumber,
-			},
-			group: ['status'],
-		});
+  try {
+    const result = await Order.findAll({
+      attributes: [
+        "status",
+        [Sequelize.fn("COUNT", Sequelize.col("id")), "total_orders"],
+        [
+          Sequelize.fn("JSON_AGG", Sequelize.literal('ROW_TO_JSON("order")')),
+          "orders",
+        ],
+      ],
+      where: {
+        createdAt: {
+          [Op.between]: [startDate, endDate],
+        },
+        user_number: userNumber,
+      },
+      group: ["status"],
+    });
 
-		return result;
-	} catch (error) {
-		console.error('Error fetching order stats:', error);
-		throw error;
-	}
+    return result;
+  } catch (error) {
+    console.error("Error fetching order stats:", error);
+    throw error;
+  }
 };
 
 export default { getAllReport, getReportByUser };
--- src/routers/router.order.js
+++ src/routers/router.order.js
@@ -1,21 +1,21 @@
-import Router from 'express';
-import orderControllers from '../controllers/controller.order.js';
-import { authenticateToken } from '../middlewares/verfiy.js';
+import Router from "express";
+import orderControllers from "../controllers/controller.order.js";
+import { authenticateToken } from "../middlewares/verfiy.js";
 const order = Router();
 
-order.get('/', authenticateToken, orderControllers.getAll);
-order.get('/by/:id', authenticateToken, orderControllers.getById);
+order.get("/", authenticateToken, orderControllers.getAll);
+order.get("/by/:id", authenticateToken, orderControllers.getById);
 order.get(
-	'/filter-code',
-	// authenticateToken,
-	orderControllers.getOrdersByProductCode
+  "/filter-code",
+  // authenticateToken,
+  orderControllers.getOrdersByProductCode
 );
 order.get(
-	'/get-by-user-name',
-	authenticateToken,
-	orderControllers.getOrderByUserName
+  "/get-by-user-name",
+  authenticateToken,
+  orderControllers.getOrderByUserName
 );
-order.post('/add', authenticateToken, orderControllers.create);
-order.put('/update/:id', authenticateToken, orderControllers.update);
+order.post("/add", orderControllers.create);
+order.put("/update/:id", authenticateToken, orderControllers.update);
 
 export default order;
