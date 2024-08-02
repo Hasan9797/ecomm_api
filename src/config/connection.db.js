c4052ca0a91f18e603e4a086c6faeed8e8a6b54b
hasan97 <hasanjonsadullyev9@gmail.com>
2024-08-02 10:35:30 +0500

    sdsds

--- src/config/connection.db.js
+++ src/config/connection.db.js
@@ -1,26 +1,28 @@
 import { Sequelize } from "sequelize";
-import env from "dotenv";
-env.config();
+import dotenv from "dotenv";
+dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    define: {
-      timestamps: true, // Globally enable timestamps
-      underscored: true, // Convert camelCase to snake_case in database columns
-      createdAt: "created_at", // Rename createdAt to created_at
-      updatedAt: "updated_at", // Rename updatedAt to updated_at
-    },
-  }
-);
+const dbName = process.env.DB_NAME;
+const sbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+
+// Connect to PostgreSQL database
+export const sequelize = new Sequelize(dbName, sbUser, String(dbPassword), {
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  port: process.env.DB_PORT,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  define: {
+    timestamps: true, // Globally enable timestamps
+    underscored: true, // Convert camelCase to snake_case in database columns
+    createdAt: "created_at", // Rename createdAt to created_at
+    updatedAt: "updated_at", // Rename updatedAt to updated_at
+  },
+});
