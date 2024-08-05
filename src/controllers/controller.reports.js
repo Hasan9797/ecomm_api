f8b8dc4b2e55b7071d830682549f706589ce0dad
hasan97 <hasanjonsadullyev9@gmail.com>
2024-08-05 11:57:06 +0500

    sdwewd

--- src/controllers/controller.reports.js
+++ src/controllers/controller.reports.js
@@ -1,45 +1,53 @@
 import serverReports from "../services/service.reports.js";
 import { dateHelper } from "../helpers/dateHelper.js";
 
-const getAllReports = async (req, res) => {
+const getAllReports = async (req, res, next) => {
   const from = req.query.from || Math.floor(Date.now() / 1000) - 84600;
   const to = req.query.to || Math.floor(Date.now() / 1000);
 
-  const result = await serverReports.getUsersReports(
-    parseInt(from),
-    parseInt(to)
-  );
+  try {
+    const result = await serverReports.getUsersReports(
+      parseInt(from),
+      parseInt(to)
+    );
 
-  if (result) {
-    return res.status(200).json({
-      message: "successfully",
-      data: result,
-    });
+    if (result) {
+      return res.status(200).json({
+        message: "successfully",
+        data: result,
+      });
+    }
+    res.status(404).json({ message: "Error" });
+  } catch (error) {
+    next(error);
   }
-  res.status(404).json({ message: "Error" });
 };
 
-const getUserReport = async (req, res) => {
+const getUserReport = async (req, res, next) => {
   const from = req.query.from || Math.floor(Date.now() / 1000) - 84600;
   const to = req.query.to || Math.floor(Date.now() / 1000);
   const userNumber = req.query.userNumber;
 
-  const result = await serverReports.getUserReport(
-    parseInt(from),
-    parseInt(to),
-    userNumber
-  );
+  try {
+    const result = await serverReports.getUserReport(
+      parseInt(from),
+      parseInt(to),
+      userNumber
+    );
 
-  if (result) {
-    return res.status(200).json({
-      message: "successfully",
-      data: result,
-    });
+    if (result) {
+      return res.status(200).json({
+        message: "successfully",
+        data: result,
+      });
+    }
+    res.status(404).json({ message: "Error" });
+  } catch (error) {
+    next(error);
   }
-  res.status(404).json({ message: "Error" });
 };
 
-const getProductsInCountMaxByOrder = async (req, res) => {
+const getProductsInCountMaxByOrder = async (req, res, next) => {
   try {
     const result = await serverReports.getProductsInCountMaxByNewOrder();
     res.status(200).json({ success: true, data: result });
@@ -49,7 +57,7 @@ const getProductsInCountMaxByOrder = async (req, res) => {
   }
 };
 
-const getUsersInCountMaxByOrder = async (req, res) => {
+const getUsersInCountMaxByOrder = async (req, res, next) => {
   try {
     const result = await serverReports.getUsersNameBySuccessOrder();
     res.status(200).json({ success: true, data: result });
